e633a7e89b45eb788631cf79c7ea4749bf62d9cf
PoornachanderReddy <poornachander295@gmail.com>
2025-08-15 16:30:25 +0530

    comments added

--- src/routes/verificationOTP.route.js
+++ src/routes/verificationOTP.route.js
@@ -9,6 +9,10 @@ const router = express.Router();
 // It sends an OTP to the user's email and verifies it
 
 router.post('/sendOtp',sendOTP);
+// This is used to verify the OTP sent to the user's email
+// It checks if the OTP is valid and marks the user as verified
+// If the OTP is valid, *it creates the user's profile* and returns a success response
+// If the OTP is invalid, it returns an error
 router.post('/verifyOtp', verifyOTP);
 export default router;
 
